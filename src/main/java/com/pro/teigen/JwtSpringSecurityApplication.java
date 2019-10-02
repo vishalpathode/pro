d30f07c5c500b904948016f8d06eba9ca48b4f0a
Vishal Pathode <vpathode@XEBIAINDIA.LOCAL>
2019-10-02 22:32:36 +0530

    remed line

--- src/main/java/com/pro/teigen/JwtSpringSecurityApplication.java
+++ src/main/java/com/pro/teigen/JwtSpringSecurityApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class JwtSpringSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JwtSpringSecurityApplication.class, args);
 	}
