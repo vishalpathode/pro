37a3040744df91299f7b35b7b7f442a59482b00f
vishalpathode <vishalpathode@hotmail.com>
2019-10-02 23:24:13 +0530

    Update JwtSpringSecurityApplication.java
    added sem

--- src/main/java/com/pro/teigen/JwtSpringSecurityApplication.java
+++ src/main/java/com/pro/teigen/JwtSpringSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.CorsFilter;;
 
 @SpringBootApplication
 public class JwtSpringSecurityApplication {
