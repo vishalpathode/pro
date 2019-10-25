87d38510e7098b82e628ba582507fd4656b1bd8e
Vishal Pathode <vpathode@XEBIAINDIA.LOCAL>
2019-10-25 17:28:06 +0530

    removed transactional

--- src/main/java/com/pro/teigen/dao/UserDaoImpl.java
+++ src/main/java/com/pro/teigen/dao/UserDaoImpl.java
@@ -5,7 +5,6 @@ import com.pro.teigen.repository.UserRepository;
 import com.pro.teigen.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
